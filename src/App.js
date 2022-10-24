ad505fc6779cc3be7a892086ceec1bec429d50d5
Mokány Ádám <mokanyadam@gmail.com>
2022-10-24 22:32:33 +0200

    - search input field - fetch management - response management

--- src/App.js
+++ src/App.js
@@ -1,44 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [location, setLocation] = useState("Budapest");
+  const [data, setData] = useState("");
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=5df8784b76b421fabc90a1eaf2fac2be';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=hu&appid=5df8784b76b421fabc90a1eaf2fac2be`;
 
-const searchLocation = (event) => {
-  if(event.key === 'Enter'){
-    
-  }
-}
+  const searchLocation = async (event) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("The server is unavailable");
+      }
+      const responseData = await response.json();
+      setData(responseData);
+
+      console.log(data);
+    } catch (error) {}
+    setLocation("");
+  };
+
+  const locationHandler = (event) => {
+    setLocation(event.target.value);
+  };
+
+  useEffect(() => {
+    searchLocation();
+  }, []);
+
+  const iconCode = data.weather ? data.weather[0].icon : null;
+  const iconValid = "http://openweathermap.org/img/wn/" + iconCode + "@4x.png";
+
+  let sunset = data.sys ? data.sys.sunset : null;
+  let sunrise = data.sys ? data.sys.sunrise : null;
+
+
+  let sunriseHours = new Date(sunrise * 1000).getHours();
+  let sunriseMinutes = new Date(sunrise * 1000).getMinutes();
+  let sunsetHours = new Date(sunset * 1000).getHours();
+  let sunsetMinutes = new Date(sunset * 1000).getMinutes();
+
+  let sunriseHours2 = new Date(sunrise * 1000).toTimeString();
+  console.log(sunriseHours2)
+
+  const sunriseTime = "Sunrise:  " + sunriseHours + ':' + sunriseMinutes;
+  const sunsetTime = "Sunset:  " + sunsetHours + ':' + sunsetMinutes;
+
 
   return (
-    <div className='app'>
-      <div className='container'>
-        <div className='top'>
-          <div className='location'>
-            <h2>Budapest</h2>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          type="text"
+          onChange={locationHandler}
+          placeholder="Enter location"
+        ></input>
+        <button onClick={searchLocation}>Search</button>
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <h2>{data.name}</h2>
           </div>
-          <div className='temp'>
-            <h1>30°</h1>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()} C°</h1> : null}
           </div>
-          <div className='description'>
-            <h3>Clouds</h3>
+          <div className="description">
+            {data.weather ? <h3>{data.weather[0].main}</h3> : null}
+          </div>
+          <div className="weatherIcon">
+            {data.weather ? <img className="icon" src={iconValid}></img> : null}
+          </div>
+          <div className="sun">
+            {data.sys ? <h3>{sunriseTime}</h3> : null}
+            {data.sys ? <h3>{sunsetTime}</h3> : null}
           </div>
         </div>
-        <div className='bottom'>
-          <div className='feels'>
-            <p>32°</p>
-            <p>Feels Like</p>
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p>{data.main.feels_like.toFixed()}</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p>{data.wind.speed.toFixed(2)} km/h</p> : null}
+              <p>Wind speed</p>
+            </div>
           </div>
-          <div className='humidity'>
-            <p>20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className='wind'>
-            <p>12km/h</p>
-            <p>Wind speed</p>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
